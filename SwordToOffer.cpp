2fe3db27515ca7f70ebd2448c63620b25a3d75e7
DamonWang888 <1225228598@qq.com>
2021-06-09 20:15:02 +0800

    最小栈

--- SwordToOffer.cpp
+++ SwordToOffer.cpp
@@ -126,6 +126,45 @@ struct point{
         return v1[0]<v2[0]?true:false;
     }
 
+/**
+ * 
+ * **/
+class MinStack {
+public:
+    /** initialize your data structure here. */
+    vector<int> elements;
+
+    MinStack() {
+        elements.clear();
+    }
+    
+    void push(int val) {
+        elements.push_back(val);
+    }
+    
+    void pop() {
+        elements.pop_back();
+    }
+    
+    int top() {
+        int elem=elements.back();
+        return elem;
+    }
+    
+    int getMin() {
+        return *std::min_element(elements.begin(),elements.end());
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
+
 class Solution
 {
 public:
