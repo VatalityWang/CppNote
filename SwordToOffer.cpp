0c1af75803618e06e5edb48da9e823e47169d95f
DamonWang888 <1225228598@qq.com>
2021-07-14 09:36:52 +0800

    int minCostClimbingStairs(vector<int>& cost) {

--- SwordToOffer.cpp
+++ SwordToOffer.cpp
@@ -257,6 +257,23 @@ public:
 class Solution
 {
 public:
+
+    /**
+     * 最小花费跑楼梯
+     * **/
+     int minCostClimbingStairs(vector<int>& cost) {
+        int i;
+        int n=cost.size();
+        vector<int> dp(3);
+        dp[0]=0;
+        dp[1]=0;
+        for(i=2;i<=n;i++){
+            dp[2]=min(dp[1]+cost[i-1],dp[0]+cost[i-2]);
+            dp[0]=dp[1];
+            dp[1]=dp[2];
+        }
+        return dp[2];
+    }
     
     /**
      * 目标和
