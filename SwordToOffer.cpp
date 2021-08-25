cf7a7edd49250bb6f2a4b34131254bb0f172e3fc
DamonWang888 <1225228598@qq.com>
2021-08-25 10:46:53 +0800

    516. 最长回文子序列

--- SwordToOffer.cpp
+++ SwordToOffer.cpp
@@ -572,7 +572,7 @@ public:
         return flag*res;
 
     }
-};
+
 
     /**
      * 24. 两两交换链表中的节点
@@ -3765,6 +3765,29 @@ public:
             }
         }
         return s.substr(start,end-start+1);
+    }
+
+    /**
+     * 516. 最长回文子序列
+     * **/
+     int longestPalindromeSubseq(string s) {
+        if(s.size()==1)
+            return 1;
+        int i,j;
+        int size=s.size();
+        
+        vector<vector<int>> dp(size,vector<int>(size));
+        for(i=size-1;i>=0;i--){
+            dp[i][i]=1;
+            for(j=i+1;j<size;j++){
+                if(s[i]==s[j]){
+                    dp[i][j]=dp[i+1][j-1]+2;
+                }else{
+                    dp[i][j]=max(dp[i+1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[0][size-1];
     }
 
     /**
