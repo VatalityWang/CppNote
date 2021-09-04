e34b354d7b73577824b5d7eb611be43836c1271c
DamonWang888 <1225228598@qq.com>
2021-09-04 21:18:35 +0800

    494. 目标和 一维 0 1 背包

--- SwordToOffer.cpp
+++ SwordToOffer.cpp
@@ -3573,7 +3573,6 @@ public:
         int i,j,n;
         n=nums.size();
      
-
         /*
         * left-right=target; left+right=sum; left=(sum+target)/2;
         */
@@ -3581,7 +3580,6 @@ public:
         if(temp%2)
             return 0;
         int s=(temp)/2; 
-
 
         //定义dp[i]: 装满容量为i的背包的总装法。
         vector<int> dp(s+1);
@@ -3597,6 +3595,29 @@ public:
 
         return dp[s];
 
+    }
+
+    //更快解法
+    int findTargetSumWays___(vector<int>& nums, int S) {
+       /*转化为01背包问题 */
+       int sum = 0;
+       for (int i = 0; i < nums.size(); i++)
+           sum += nums[i];
+       if (abs(S) > abs(sum))
+           return 0; // 此时没有⽅案
+       if ((S + sum) % 2 == 1)
+           return 0; // 此时没有⽅案
+       int bagSize = (S + sum) / 2;
+       vector<int> dp(bagSize + 1, 0);
+       dp[0] = 1;
+       for (int i = 0; i < nums.size(); i++)
+       {
+           for (int j = bagSize; j >= nums[i]; j--)
+           {
+               dp[j] += dp[j - nums[i]];
+           }
+       }
+       return dp[bagSize];
     }
 
     /**
