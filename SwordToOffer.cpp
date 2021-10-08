cec1b275ce9cf78a40e3f583abd79b9d2d599a28
DamonWang888 <1225228598@qq.com>
2021-10-08 16:57:16 +0800

    118. 杨辉三角

--- SwordToOffer.cpp
+++ SwordToOffer.cpp
@@ -734,6 +734,30 @@ class Solution
 public:
 
     /**
+     * 118. 杨辉三角
+     * **/
+     vector<vector<int>> generate(int numRows) {
+
+        vector<vector<int>> res(numRows);
+
+        if(numRows==1)
+            return {{1}};
+
+
+        res[0].push_back(1);
+
+        for(int i=1;i<numRows;i++){
+            res[i].push_back(1);
+            for(int j=0;j<res[i-1].size()-1;j++)
+                res[i].push_back(res[i-1][j]+res[i-1][j+1]);
+            res[i].push_back(1);
+        }
+
+        return res;
+
+    }
+
+    /**
      * 187. 重复的DNA序列
      * **/
     vector<string> findRepeatedDnaSequences(string s) {
