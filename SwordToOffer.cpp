32bfd75d19b933e61be731e21ad2ad14a7c57215
DamonWang888 <1225228598@qq.com>
2021-11-24 14:14:58 +0800

    718. 最长重复子数组

--- SwordToOffer.cpp
+++ SwordToOffer.cpp
@@ -771,6 +771,27 @@ class Solution
 {
 public:
 
+
+    /**
+     * 718. 最长重复子数组
+     * **/
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
+        int m=nums1.size();
+        int n=nums2.size();
+
+        //dp[i][j]:以A[i-1],B[j-1]结尾的两个数组的最长公共子数组长度
+        vector<vector<int>> dp(m+1,vector<int>(n+1));
+
+        int res=0;
+        for(int i=1;i<=nums1.size();i++)
+            for(int j=1;j<=nums2.size();j++){
+                    if(nums1[i-1]==nums2[j-1])
+                        dp[i][j]=dp[i-1][j-1]+1;
+                    res=max(res,dp[i][j]);  
+            }
+        return res;
+    }
+
     /**
      * 674. 最长连续递增序列
      * **/
